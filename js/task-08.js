2ef47332e3074cb886703d31d62095020866438b
Maksym Chubarov <maximuskh@gmail.com>
2020-11-14 18:20:59 +0200

    2

--- js/task-08.js
+++ js/task-08.js
@@ -1,32 +1,35 @@
-// function createBoxes ()  { 
-//     const listing = document.createElement('div');
-//     div.append(listing)
-// function destroyBoxes ()  { 
-//     div.remove(listing)
-// }
-
-
-      
-  const div = document.querySelector("#boxes")
-  
+const div = document.querySelector("#boxes")
   const btnRender = document.querySelector('button[data-action="render"]');
   const btnDestroy = document.querySelector('button[data-action="destroy"]');
-  
-   
-  btnRender.addEventListener('click', ()=> {
-    const listing = document.createElement('div');
+  btnRender.addEventListener('click', getAmount);
+  btnDestroy.addEventListener('click', destroyBoxes);
 
-    console.log(listing)
-    div.append(listing)
-  });
-  
-  
-  
-  btnDestroy.addEventListener('click', () =>{
-    console.log('listing')
-    div.removeChild(listing)
+function getAmount() {
+  const amount = +document.querySelector("#controls input").value;
+  console.log(amount)
+  createBoxes(amount);
+}
 
-  });
+
+function createBoxes(amount) {
+  
+  const fragment = document.createDocumentFragment();
+  
+  for (let i = 0; i < amount; i++) {
+     const listin = document.createElement("div");
+     fragment.appendChild(listin);
+     div.appendChild(fragment);
+}
+
+      
+} 
+  
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+  
+  
+  
 
 
   
